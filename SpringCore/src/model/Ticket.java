3756da3f7a654f2b467ff0abb8ed24cf8819946a
Pavlo Khodachok <Pavlo_Khodachok@epam.com>
2021-11-07 14:55:02 +0200

    Added spring core module.

---
+++ SpringCore/src/model/Ticket.java
@@ -0,0 +1,21 @@
+package model;
+
+public interface Ticket {
+    public enum Category {STANDARD, PREMIUM, BAR}
+
+    /**
+     * Ticket Id. UNIQUE.
+     * @return Ticket Id.
+     */
+    long getId();
+    void setId(long id);
+    long getEventId();
+    void setEventId(long eventId);
+    long getUserId();
+    void setUserId(long userId);
+    Category getCategory();
+    void setCategory(Category category);
+    int getPlace();
+    void setPlace(int place);
+
+}
---
+++ SpringCore/src/model/impl/Event.java
@@ -0,0 +1,39 @@
+package model.impl;
+
+import java.util.Date;
+
+public class Event implements model.Event {
+    private long id;
+    private String title;
+    private Date date;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
